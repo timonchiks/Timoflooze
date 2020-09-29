7160ac31ba5a17b84c1f72a7d2e7cef0016c491a
valerloozetimof <tim.1010@yandex.ru>
2020-09-29 17:21:53 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -26,17 +26,15 @@ signed main() {
 	inlook A2;
 	door A3;
 	while (1) {
-		std::cout << "Выберите цифру, \n 1 - цвет дверей \n 2 - размер дверей \n 3 - размер авто \n 4 - цвет авто \n 5 - тип авто \n 6 - обивка кожей \n 7 - кондиционер \n 8- навигатор";
-		int a, b;
 		std::cin >> a;
-		(a == 1) ? (std::cin >> A3.color) : (b = 0);
-		a == 2 ? std::cin >> A3.size : b = 0;
-		a == 3 ? std::cin >> A1.color : b = 0;
-		a == 4 ? std::cin >> A1.size : b = 0;
-		a == 5 ? std::cin >> A1.type : b = 0;
-		a == 6 ? std::cin >> A2.skin : b = 0;
-		a == 7 ? std::cin >> A2.freezer : b = 0;
-		a == 8 ? std::cin >> A2.navigator : b = 0;
+		if (a == 1) std::cin >> A3.color;
+		if (a == 2) std::cin >> A3.size;
+		if (a == 3) std::cin >> A1.color;
+		if (a == 4) std::cin >> A1.size;
+		if (a == 5) std::cin >> A1.type;
+		if (a == 6) std::cin >> A2.skin;
+		if (a == 7) std::cin >> A2.freezer;
+		if (a == 8) std::cin >> A2.navigator;
 	}
 	return 0;
 }
