25ceb136d3eb76c21fdc70986844693a0e8b20cc
valerloozetimof <tim.1010@yandex.ru>
2020-09-29 18:03:03 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,44 +1,21 @@
-#include <string>
-#include <iostream>
-using namespace std;
+#include <D:/Visual_Studio/stdc++.h>
 
-class door {
+class tree {
 public:
-	std::string color;
-	int size;
-};
-
-class body {
-public:
-	int size;
-	std::string color;
-	std::string type;
-};
-class inlook {
-public:
-	bool skin = false;
-	bool freezer = false;
-	bool navigator = false;
-};
-class car {
-public:
-	body A1;
-	inlook A2;
-	door A3;
+	tree obj;
+	tree box;
+	bool full = false;
+	tree dod(tree ob) {
+		if (ob.full == false) break;
+		else {
+			dod(ob.obj);
+			dod(ob.box);
+		}
+	}
 };
 
 signed main() {
-	car toyota;
-	while (1) {
-		std::cin >> a;
-		if (a == 1) std::cin >> toyota.A3.color;
-		if (a == 2) std::cin >> A3.size;
-		if (a == 3) std::cin >> A1.color;
-		if (a == 4) std::cin >> A1.size;
-		if (a == 5) std::cin >> A1.type;
-		if (a == 6) std::cin >> A2.skin;
-		if (a == 7) std::cin >> A2.freezer;
-		if (a == 8) std::cin >> A2.navigator;
-	}
+	tree a;
+	a.dod(a);
 	return 0;
 }
